b5c42869e4d05f66e062f56e2bd19b5b91fcc8bc
ffd8 <ted@ffd8.org>
2025-06-27 01:25:10 +0200

    updates
    - sonicode fixes (pitch, active time, midi)
    - snippets, built-in reduced because:
    - snippets manager added! separate repo with access to add
    - ghostmode snippets added, types out snippets w set speed
    - ghostmode snippets, added undo stack so entire typing is undo 1x
    - ghostmode added to ace snippets
    - snippets adds libs to existing array if present!
    - added more stats
    - darkmode ace find GUI
    -

--- includes/utils/ace.ghost.js
+++ includes/utils/ace.ghost.js
@@ -11,11 +11,16 @@ class AceGhost{
 	constructor(editor){
 		this.que = []
 		this.editor = editor
-		this.showTextTimeout
+		this.animateTextTimeout
 		this.delay = 10
+		this.debug = false
 	}
 
-	showText(message, index){
+	log(txt){
+		if(this.debug) console.log(txt)
+	}
+
+	animateText(message, index, cb){
 		if (index < message.length) {
 		 	// insert at cursor
 			this.editor.session.insert(this.editor.getCursorPosition(), message[index++]);
@@ -27,13 +32,17 @@ class AceGhost{
 			// adjust delay after typing a space
 			// if (/\s/.test(message[index - 1])) delay = 0
 			var self = this
-			this.showTextTimeout = setTimeout(function () {
-				this.showText(message, index);
+			this.animateTextTimeout = setTimeout(function () {
+				this.animateText(message, index, cb);
 			}.bind(self), this.delay);
 		}else{
-			tidyCode(editor);
+
+			if(cb){
+				cb()
+			}
+			// tidyCode(editor);
 			let addDelay = false
-			if(['globalTop', 'setup', 'globalBottom'].includes(this.que[0].funName) && this.que[0].codeInsert != ''){
+			if(this.que.length > 0 && ['globalTop', 'setup', 'globalBottom'].includes(this.que[0].funName) && this.que[0].codeInsert != ''){
 				recompile(true);
 				addDelay = true
 			}
@@ -42,6 +51,8 @@ class AceGhost{
 				this.que.splice(0, 1)
 
 				setTimeout(()=>{
+					// this.setUndoStack()
+					// this.getUndoStack()
 					this.addCode(this.que[0])
 				}, addDelay ? 1000:0)
 				
@@ -50,17 +61,17 @@ class AceGhost{
 				if(!settings.cocoding.active){
 					localStorage[settings.fileName] = editor.getValue();
 				}
-				// console.log('snippet added')
-				// this.editor.session.$undoManager.markIgnored(this.rev); // mark the new group as ignored
+
+				this.setUndoStack()
 			}
 		}
 	}
 
-	insertText(txt){
+	insertText(txt, cb){
   		// editor.setCursorPosition(pos)
  		// editor.moveCursorTo(pos.row, pos.column); //lineNumber,colNumber
 		this.disableEditor(true)
- 		this.showText(txt, 0, this.delay)
+ 		this.animateText(txt, 0, cb)
 	}
 
 	disableEditor(state){
@@ -80,6 +91,17 @@ class AceGhost{
 		let curPos = this.editor.getCursorPosition()
 		let curLineText = this.editor.session.getLine(curPos.row);
 
+		let curCode = editor.getValue()
+		if(snippetCode.hasOwnProperty('libs')){
+			let curLibs = curCode.match(/(?=var|let|const).*(?=loadScripts|libs|scripts).*?(\])/gs)[0]
+			let newLibs = curLibs.slice(0, -1) + snippetCode.libs.map(lib => `,\n'${lib}'`).join('') + ',' + curLibs.slice(-1)
+			curCode = curCode.replace(curLibs, `\n${newLibs}\n`)
+		}
+
+		this.getUndoStack()
+
+		editor.setValue(curCode, 0)
+
 		this.addQue(snippetCode.gt, 'globalTop', 'top');
 		this.addQue(snippetCode.s, 'setup', 'bottom');
 		this.addQue(snippetCode.dt, 'draw', 'top');
@@ -89,13 +111,9 @@ class AceGhost{
 		// console.log(que)
 		this.disableEditor(true)
 
-		// https://github.com/ajaxorg/ace/issues/3743#issuecomment-412334489
-		// this.rev = this.editor.session.$undoManager.startNewGroup(); // start new undo group
-
 		setTimeout(()=>{
 			this.addCode(this.que[0])
 		}, 200)
-		
 		
 		// return cursor to previous position (** fix if blank line)
 		let curLines = this.editor.getValue().split('\n')
@@ -104,6 +122,41 @@ class AceGhost{
 			this.editor.moveCursorTo(curRow, curPos.column);
 			this.editor.renderer.scrollCursorIntoView({row: curRow, column: curPos.column}, 0.5);
 		}		
+	}
+
+	getUndoStack(){
+		// *** start undostack collecting
+		this.log('getUndoStack')
+		this.undostackBefore = this.editor.session.$undoManager.$undoStack
+		// this.log(this.undostackBefore)
+		this.editor.session.$undoManager.reset();
+	}
+
+	setUndoStack(){
+		// *** end collecting
+		this.log('setUndoStack')
+		this.undostackAfter = this.editor.session.$undoManager.$undoStack
+		// this.log(this.undostackAfter)
+		let newStack = [], lastDelta, lastDeltas
+		for(let d of this.undostackBefore){
+			newStack.push(d)
+		}
+
+		let comboStack = []
+		for(let deltas of this.undostackAfter){
+			for(let d of deltas){
+				comboStack.push(d)
+				lastDelta = d
+			}
+			lastDeltas = deltas
+		}
+
+		newStack.push(comboStack)
+		this.editor.session.$undoManager.$undoStack = newStack
+		this.editor.session.$undoManager.$maxRev = newStack.length
+		this.editor.session.$undoManager.$rev = newStack.length
+		this.editor.session.$undoManager.$lastDelta = lastDelta
+		this.editor.session.$undoManager.$lastDeltas = lastDeltas
 	}
 
 	addQue(codeInsert, funName, codePos){
@@ -144,7 +197,7 @@ class AceGhost{
 		}
 
 		// editor.session.insert(editor.getCursorPosition(), insertCode)
-		this.insertText(codeInsert, 10)
+		this.insertText(codeInsert)
 	}
 
 	// grab start/end of function, modded for position only
